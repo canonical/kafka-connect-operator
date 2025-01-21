0369afe6447ef35b90ff921bc88708a6e6eca3b9
Iman Enami <iman.enami@canonical.com>
2025-01-21 15:47:36 +0400

    add TODO for LOG_SENSITIVE_OUTPUT

--- src/literals.py
+++ src/literals.py
@@ -40,6 +40,7 @@ REPLICATION_FACTOR = -1  # -1 uses broker's default replication factor
 KAFKA_CLIENT_REL = "kafka-client"
 PEER_REL = "worker"
 
+# TODO: this should be set using `profile` config option in the future
 LOG_SENSITIVE_OUTPUT = True  # set False for production mode & builds
 
 DebugLevel = Literal["DEBUG", "INFO", "WARNING", "ERROR"]
