8722d675e69a587a435aaee5cc94ce34ae41d78a
Iman Enami <iman.enami@canonical.com>
2025-02-05 14:47:21 +0400

    fix: int tests conftest

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -18,6 +18,6 @@ async def kafka_connect_charm(ops_test: OpsTest):
 @pytest.fixture(scope="module")
 async def integrator_charm(ops_test: OpsTest):
     """Build the integrator charm."""
-    charm_path = "integrator/."
+    charm_path = "./integrator/"
     charm = await ops_test.build_charm(charm_path)
     return charm
