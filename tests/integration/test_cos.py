5abb4ca50c0dc64a99d65cc5bb452dd580e41d0d
Iman Enami <iman.enami@canonical.com>
2025-02-19 20:17:16 +0400

    feat: add microk8s fixture

---
+++ tests/integration/test_cos.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import logging
+import os
+
+import pytest
+from pytest_operator.plugin import OpsTest
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.abort_on_fail
+@pytest.mark.skip_if_deployed
+async def test_deploy_charms(ops_test: OpsTest, juju_microk8s):
+    logger.info("Microk8s controller set up successfully!")
+
+    os.system("juju switch microk8s-localhost")
+    os.system("juju models")
