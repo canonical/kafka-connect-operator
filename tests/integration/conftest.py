60b6709f54f31dc076a68cb923d09da625db31b2
Iman Enami <iman.enami@canonical.com>
2025-02-20 14:50:04 +0400

    feat: add grafana tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -5,11 +5,9 @@
 
 import asyncio
 import logging
-import os
-import subprocess
 
 import pytest
-from deployment import Deployment
+from helpers import Testbed
 from juju.errors import JujuConnectionError
 from pytest_operator.plugin import OpsTest
 
@@ -32,67 +30,29 @@ async def integrator_charm(ops_test: OpsTest):
     return charm
 
 
-@pytest.fixture(scope="session")
-def deployment():
-    return Deployment()
+@pytest.fixture(scope="module")
+def testbed(ops_test: OpsTest):
+    return Testbed(ops_test=ops_test)
 
 
 @pytest.fixture(scope="module")
-def microk8s_controller(deployment: Deployment):
+def microk8s_controller(testbed: Testbed):
     """Returns the microk8s controller name, boots up a new one if not existent."""
-    if deployment.microk8s_controller:
-        logger.info(
-            f"Microk8s controller {deployment.microk8s_controller} exists, skipping setup..."
-        )
-        return deployment.microk8s_controller
+    if testbed.microk8s_controller:
+        logger.info(f"Microk8s controller {testbed.microk8s_controller} exists, skipping setup...")
+        return testbed.microk8s_controller
 
-    user_env_var = os.environ.get("USER", "root")
-    os.system("sudo apt install -y jq")
-    ip_addr = subprocess.check_output(
-        "ip -4 -j route get 2.2.2.2 | jq -r '.[] | .prefsrc'",
-        universal_newlines=True,
-        stderr=subprocess.PIPE,
-        shell=True,
-    ).strip()
-
-    deployment.run_script(
-        f"""
-        # install microk8s
-        sudo snap install microk8s --classic --channel=1.32
-
-        # configure microk8s
-        sudo usermod -a -G microk8s {user_env_var}
-        mkdir -p ~/.kube
-        chmod 0700 ~/.kube
-
-        # ensure microk8s is up
-        sudo microk8s status --wait-ready
-
-        # enable required addons
-        sudo microk8s enable dns
-        sudo microk8s enable hostpath-storage
-        sudo microk8s enable metallb:{ip_addr}-{ip_addr}
-
-        # configure & bootstrap microk8s controller
-        sudo mkdir -p /var/snap/juju/current/microk8s/credentials
-        sudo microk8s config | sudo tee /var/snap/juju/current/microk8s/credentials/client.config
-        sudo chown -R {user_env_var}:{user_env_var} /var/snap/juju/current/microk8s/credentials
-
-        juju bootstrap microk8s
-        sleep 90
-    """
-    )
-
+    testbed.bootstrap_microk8s()
     return "microk8s-localhost"
 
 
 @pytest.fixture(scope="module")
-async def cos_lite(microk8s_controller: str, deployment: Deployment):
+async def cos_lite(microk8s_controller: str, testbed: Testbed):
     """Returns the COS-lite model, deploys a new one if not existent."""
     cos_model_name = "cos"
 
     try:
-        model = await deployment.get_model(microk8s_controller, cos_model_name)
+        model = await testbed.get_model(microk8s_controller, cos_model_name)
         yield model
 
         await model.disconnect()
@@ -100,7 +60,7 @@ async def cos_lite(microk8s_controller: str, deployment: Deployment):
     except JujuConnectionError:
         logger.info(f"Model {cos_model_name} doesn't exist, trying to deploy...")
 
-    deployment.run_script(
+    testbed.run_script(
         f"""
         # deploy COS-Lite
         juju switch {microk8s_controller}
@@ -117,7 +77,7 @@ async def cos_lite(microk8s_controller: str, deployment: Deployment):
     )
 
     await asyncio.sleep(60)
-    model = await deployment.get_model(microk8s_controller, cos_model_name)
+    model = await testbed.get_model(microk8s_controller, cos_model_name)
 
     await model.wait_for_idle(status="active", idle_period=60, timeout=3000, raise_on_error=False)
     yield model
@@ -126,12 +86,12 @@ async def cos_lite(microk8s_controller: str, deployment: Deployment):
 
 
 @pytest.fixture(scope="module")
-async def test_model(ops_test: OpsTest, deployment: Deployment):
+async def test_model(ops_test: OpsTest, testbed: Testbed):
     """Returns the ops_test model on lxd cloud."""
-    if not deployment.lxd_controller or not ops_test.model:
+    if not testbed.lxd_controller or not ops_test.model:
         raise Exception("Can't communicate with the controller.")
 
-    model = await deployment.get_or_create_model(deployment.lxd_controller, ops_test.model.name)
+    model = await testbed.get_or_create_model(testbed.lxd_controller, ops_test.model.name)
     yield model
 
     await model.disconnect()
--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -2,7 +2,10 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import asyncio
 import json
+import logging
+import os
 import re
 import socket
 import ssl
@@ -11,7 +14,7 @@ from contextlib import asynccontextmanager, closing
 from dataclasses import dataclass
 from pathlib import Path
 from subprocess import PIPE, check_output
-from typing import cast
+from typing import Optional, cast
 
 import requests
 import yaml
@@ -20,12 +23,17 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.x509 import Certificate, load_pem_x509_certificate
 from cryptography.x509.extensions import SubjectAlternativeName
 from cryptography.x509.oid import ExtensionOID
+from juju.errors import JujuConnectionError
+from juju.model import Model
 from ops.model import Unit
 from pytest_operator.plugin import OpsTest
 from requests.auth import HTTPBasicAuth
 
 from core.models import PeerWorkersContext
 from literals import CONFIG_DIR, DEFAULT_API_PORT
+
+logger = logging.getLogger(__name__)
+
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = METADATA["name"]
@@ -265,3 +273,119 @@ async def self_signed_ca(ops_test: OpsTest, app_name: str):
         ca_file.write(ca)
         ca_file.close()
         yield ca_file
+
+
+class Testbed:
+    """Fixture for handling multi-cloud juju deployments."""
+
+    def __init__(self, ops_test: OpsTest):
+        self._ops_test = ops_test
+        logging.warning("When using Testbed, avoid interacting directly with ops_test.")
+        pass
+
+    def exec(self, cmd: str) -> str:
+        """Executes a command on shell and returns the result."""
+        return check_output(
+            cmd,
+            stderr=PIPE,
+            shell=True,
+            universal_newlines=True,
+        )
+
+    def run_script(self, script: str) -> None:
+        """Runs a script on Linux OS.
+
+        Args:
+            script (str): Bash script
+
+        Raises:
+            OSError: If the script run fails.
+        """
+        for line in script.split("\n"):
+            command = line.strip()
+
+            if not command or command.startswith("#"):
+                continue
+
+            logger.info(command)
+            ret_code = os.system(command)
+
+            if ret_code:
+                raise OSError(f'command "{command}" failed with error code {ret_code}')
+
+    def juju(self, *args, json_output=True) -> dict:
+        """Runs a juju command and returns the result in JSON format."""
+        _format_json = "" if not json_output else "--format json"
+        res = self.exec(f"juju {' '.join(args)} {_format_json}")
+
+        if json_output:
+            return json.loads(res)
+
+        return {}
+
+    def get_controller_name(self, cloud: str) -> Optional[str]:
+        """Gets controller name for specified cloud, e.g. localhost, microk8s, lxd, etc."""
+        res = self.juju("controllers")
+        for controller in res.get("controllers", {}):
+            if res["controllers"][controller].get("cloud") == cloud:
+                return controller
+
+        return None
+
+    async def get_model(self, controller_name: str, model_name: str) -> Model:
+        """Gets a juju model on specified controller, raises JujuConnectionError if not found."""
+        state = await OpsTest._connect_to_model(controller_name, model_name)
+        return state.model
+
+    async def get_or_create_model(self, controller_name: str, model_name: str) -> Model:
+        """Returns an existing model on a controller or creates new one if not existing."""
+        try:
+            return await self.get_model(controller_name, model_name)
+        except JujuConnectionError:
+            self.juju("add-model", "-c", controller_name, model_name, json_output=False)
+            await asyncio.sleep(10)
+            return await self.get_model(controller_name, model_name)
+
+    def bootstrap_microk8s(self) -> None:
+        """Bootstrap juju on microk8s cloud."""
+        user_env_var = os.environ.get("USER", "root")
+        os.system("sudo apt install -y jq")
+        ip_addr = self.exec("ip -4 -j route get 2.2.2.2 | jq -r '.[] | .prefsrc'").strip()
+
+        self.run_script(
+            f"""
+            # install microk8s
+            sudo snap install microk8s --classic --channel=1.32
+
+            # configure microk8s
+            sudo usermod -a -G microk8s {user_env_var}
+            mkdir -p ~/.kube
+            chmod 0700 ~/.kube
+
+            # ensure microk8s is up
+            sudo microk8s status --wait-ready
+
+            # enable required addons
+            sudo microk8s enable dns
+            sudo microk8s enable hostpath-storage
+            sudo microk8s enable metallb:{ip_addr}-{ip_addr}
+
+            # configure & bootstrap microk8s controller
+            sudo mkdir -p /var/snap/juju/current/microk8s/credentials
+            sudo microk8s config | sudo tee /var/snap/juju/current/microk8s/credentials/client.config
+            sudo chown -R {user_env_var}:{user_env_var} /var/snap/juju/current/microk8s/credentials
+
+            juju bootstrap microk8s
+            sleep 90
+        """
+        )
+
+    @property
+    def lxd_controller(self) -> Optional[str]:
+        """Returns the lxd controller name or None if not available."""
+        return self.get_controller_name("localhost")
+
+    @property
+    def microk8s_controller(self) -> Optional[str]:
+        """Returns the microk8s controller name or None if not available."""
+        return self.get_controller_name("microk8s")
