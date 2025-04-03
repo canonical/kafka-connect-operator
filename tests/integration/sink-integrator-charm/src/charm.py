5c639b4340d142dd6be781c396c9c50701537c62
Iman Enami <iman.enami@canonical.com>
2025-04-03 12:28:27 +0400

    fix: use connector_status

--- tests/integration/sink-integrator-charm/src/charm.py
+++ tests/integration/sink-integrator-charm/src/charm.py
@@ -92,7 +92,7 @@ class TestIntegratorCharm(CharmBase):
             return
 
         try:
-            event.add_status(ActiveStatus(self.integrator.task_status))
+            event.add_status(ActiveStatus(self.integrator.connector_status))
         except Exception as e:
             logger.error(e)
             event.add_status(
